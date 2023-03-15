5a4199c6a7c07ef5cf52aac4bf3b6f65af61c635
streq <strequena@gmail.com>
2023-03-15 20:02:43 -0300

    un nivel con camara

--- src/test/ingame_rope_test/box.gd
+++ src/test/ingame_rope_test/box.gd
@@ -6,16 +6,16 @@ export var gravity := Vector2()
 export var previous_velocity := Vector2()
 
 func _physics_process(delta: float) -> void:
-	velocity += gravity*delta
-	previous_velocity = velocity
-	velocity = move_and_slide(velocity)
+#	velocity += gravity*delta
+#	previous_velocity = velocity
+#	velocity = move_and_slide(velocity)
 	
 	if get_slide_count() > 0 and previous_velocity.distance_to(velocity)>100.0:
 		die()
 	
 	if is_on_wall():
-		velocity*=(1.0-delta*5.0)
-	
+#		velocity*=(1.0-delta*5.0)
+		pass
 
 
 func die():
